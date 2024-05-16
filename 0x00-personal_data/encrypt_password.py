1e87d5518a02baba26488edc986964a060ffc992
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-16 05:09:32 +0000

    Add 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,3 +13,8 @@ def hash_password(password: str) -> bytes:
     # Hash the password with the salt
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
