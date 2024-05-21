56160327a7b0883120cd424ac09b271efb4df6ab
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-21 04:02:20 +0000

    fixed pycodestyle

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes = False)
+@app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> str:
     """Tests the unauthorized route"""
     abort(401)
