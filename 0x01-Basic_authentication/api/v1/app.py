a85b9895fdaf42a9fa31031c6ab597509103a011
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-21 04:05:16 +0000

    Add 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,9 +23,16 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def not_found(error) -> str:
-    """ Not found handler
+    """ Unauthorized error handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def not_found(error) -> str:
+    """ Forbidden error handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> str:
     """Tests the unauthorized route"""
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """Tests the forbidden route"""
+    abort(403)
