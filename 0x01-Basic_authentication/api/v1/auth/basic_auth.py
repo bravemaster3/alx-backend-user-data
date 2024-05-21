e217adbd45b4ee9944287f03f3509f8a10552ed8
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-21 14:59:00 +0000

    Add 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,17 @@
 This module defines authentication class
 """
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """class definition here"""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extracts the hashed auth string"""
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        if not re.match('^Basic ', authorization_header):
+            return None
+        return authorization_header[6:]
