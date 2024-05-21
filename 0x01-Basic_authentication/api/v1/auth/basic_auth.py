fe3e1cd69eede11e13921a1d676d4dbe96eb6c4f
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-21 17:11:14 +0000

    Add 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ This module defines authentication class
 from api.v1.auth.auth import Auth
 import re
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -31,3 +32,15 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """extracting user credentials"""
+        if not decoded_base64_authorization_header:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if not ':' in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':'))
