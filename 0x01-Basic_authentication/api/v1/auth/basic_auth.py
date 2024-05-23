ea1bc69de11772b939cc2eba567108dfd2773fd2
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-23 10:40:43 +0000

    Add trying to fix the only red check in 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,7 +59,7 @@ class BasicAuth(Auth):
             user_x = User.search({"email": user_email})
         except Exception:
             return None
-        if not user_x:
+        if not user_x or len(user_x) > 1:
             return None
         if not user_x[0].is_valid_password(user_pwd):
             return None
