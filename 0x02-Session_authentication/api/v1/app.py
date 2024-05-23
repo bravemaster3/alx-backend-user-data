2912ca2186a692625a7a1bf290d01a88f88268dd
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-23 18:40:02 +0000

    Add 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,8 +57,9 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None, abort(401)
+    if auth.authorization_header(request) and auth.session_cookie(request) is\
+         None:
+        abort(401)
     request.current_user = auth.current_user(request)
 
 
