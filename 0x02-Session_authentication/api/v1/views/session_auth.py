6e46a556daecc3bd291101db22a333bf72908844
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-24 02:40:20 +0000

    incomplete 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,10 +3,10 @@
 from flask import jsonify, make_response, request
 from api.v1.views import app_views
 from models.user import User
-from api.v1.app import auth
+import os
 
 
-@app_views.route('/login', methods=['POST'])
+@app_views.route('auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """login method"""
     email = request.form.get('email')
@@ -15,16 +15,18 @@ def login():
         return jsonify({"error": "email missing"}), 400
     if not password:
         return jsonify({"error": "password missing"}), 400
-    user = User.search(email=email)
+    user = User.search({"email": email})
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    session_id = auth.create_session(user.user_id)
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
 
     user_json = user.to_json()
 
     response = make_response(user_json)
-    response.set_cookie(auth.session_cookie_name, session_id)
+    response.set_cookie(cookie_name=os.getenv('SESSION_NAME',
+                                              '_my_session_id'), session_id)
 
     return response
