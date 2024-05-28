c2a64d3df8ff32cd7e67292d281b3c5ea61e733d
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-28 03:42:48 +0000

    Add 0 to 5

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password with bcrypt"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """initialises a database instance"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers a new user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        new_user = self._db.add_user(email, _hash_password(password))
+        return new_user
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,64 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Creates and adds a user to the database"""
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
+        try:
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            raise
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Searches for a user"""
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No result found for the given arguments")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments")
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user"""
+        my_user = self.find_user_by(id=user_id)
+        for key in kwargs:
+            if not hasattr(my_user, key):
+                raise ValueError(f"The key {key} is not a valid key of user")
+        for key, value in kwargs.items():
+            setattr(my_user, key, value)
+        self._session.commit()
