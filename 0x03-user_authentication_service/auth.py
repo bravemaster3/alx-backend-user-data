d9ac74dee89719de13ebafb9ce2eb8e6a7cf4080
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-29 03:16:21 +0000

    Add 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,3 +48,13 @@ class Auth:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """creates a session for a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
