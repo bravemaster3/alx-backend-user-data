f66695befe65a202f45504ef332934cc0cb10c61
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-29 03:45:49 +0000

    Add 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,7 @@ class Auth:
             return User
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a user's session"""
+        self._db.update_user(user_id, session_id=None)
