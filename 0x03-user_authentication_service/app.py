fa6934fd727d250739d3b91855213c3f54f7edb4
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-29 17:46:05 +0000

    Add 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,7 +96,7 @@ def update_password():
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
     try:
-        user = AUTH._db.find_user_by(email=email, reset_token=reset_token)
+        user = AUTH._db.find_user_by(email=email)
         AUTH.update_password(email, new_password)
         return jsonify(
             {"email": user.email, "message": "Password updated"}
