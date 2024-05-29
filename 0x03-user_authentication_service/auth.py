9bd3147d45d0c9e5b1e6189f42ce8c989b7e8fd4
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-29 03:40:30 +0000

    Add 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,3 +58,13 @@ class Auth:
             return session_id
         except Exception:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        """finds a user given their session id"""
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return User
+        except Exception:
+            return None
