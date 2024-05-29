7562531bd4e3d2bb15b808fa8168b271bc7e16f3
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-29 17:13:54 +0000

    Add 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -76,14 +76,17 @@ def profile():
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
     """resets password"""
-    email = request.form.get("email")
-    user = AUTH._db.find_user_by(email=email)
-    if not user:
+    try:
+        email = request.form.get("email")
+        user = AUTH._db.find_user_by(email=email)
+        if not user:
+            abort(403)
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify(
+            {"email": email, "reset_token": reset_token}
+        ), 200
+    except Exception:
         abort(403)
-    reset_token = AUTH.get_reset_password_token(email)
-    return jsonify(
-        {"email": email, "reset_token": reset_token}
-    ), 200
 
 
 if __name__ == "__main__":
