eca0ae3e588d53553d248f98843e2e08fc29e22e
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-29 03:32:17 +0000

    Add 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -12,11 +12,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    """home route"""
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
+    """registers a user"""
     email = request.form.get('email')
     password = request.form.get('password')
 
@@ -25,6 +27,22 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """logs a user in"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        response = make_response(
+            jsonify({"email": email, "message": "logged in"}), 200
+        )
+        response.set_cookie("session_id", session_id)
+        return response
 
 
 if __name__ == "__main__":
